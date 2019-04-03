f917994968d3544d57e690a0a06b79837e599bc5
XWQ2018 <18476969365@163.com>
2019-04-03 17:44:27 +0800

    增加拦截器配置

--- src/untils/request.js
+++ src/untils/request.js
@@ -1,1 +1,45 @@
-import axios from 'axios';
+import $http from 'axios';
+import Qs from 'qs';
+import { $toast } from 'vant';
+
+// process.env.VUE_APP_API_URL
+
+var service = $http.create({
+    baseURL: process.env.VUE_APP_API_URL,
+    timeout: 2000,
+    headers: { 'X-Custom-Header': 'foobar' },
+    transformResponse: [function (data) {
+        // Do whatever you want to transform the data
+        data = Qs.stringify(data);
+        return data;
+    }],
+});
+
+const tips = {
+    40000: function () {
+        $toast('xxxxx')
+    }
+}
+//interceptors.request
+service.interceptors.request.use(function (config) {
+    console.log(config)
+    if (!config.data) {
+        config.data = {};
+    }
+    config.data.token = '44646116f161689441616161';
+    return config;
+}, error => {
+    return Promise.rejiect(error)
+});
+
+//interceptors.response
+service.interceptors.response.use(function (data) {
+    console.log(data)
+
+}, error => {
+    return Promise.reject(error)
+});
+
+
+
+export default service;
