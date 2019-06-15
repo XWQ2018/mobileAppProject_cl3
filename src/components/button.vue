199d9809a1d8321e012a314daed5fb84444740cd
laoxiao <18476969365@163.com>
2019-06-15 19:07:53 +0800

    新增按钮公共组件

---
+++ src/components/button.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="page">
+        <div class="main-container">
+            <div :class="{fixed:isFixed}">
+                <van-button
+                    native-type="button"
+                    :type="typeInfo"
+                    :size="buttonSize"
+                    :icon="leftIcon"
+                    :text="buttonText"
+                    :isFixed="isFixed"
+                    :disabled="isDisabled"
+                    @click="clickButton"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        buttonText: {
+            type: String,
+            default: "确定"
+        },
+        isFixed: {
+            type: Boolean,
+            default: false
+        },
+        buttonSize: {
+            type: String,
+            default: "large"
+        },
+        leftIcon: {
+            type: String
+        },
+        typeInfo: {
+            type: String,
+            default: "info"
+        },
+        isDisabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {};
+    },
+    created() {},
+    mouted() {},
+    methods: {
+        clickButton() {
+            this.$emit("clickButton");
+        }
+    }
+};
+</script>
+<style lang="less" scoped>
+.page {
+    .main-container {
+        padding: 0;
+        .fixed {
+            position: fixed;
+            right: 0;
+            left: 0;
+            bottom: 15px;
+            padding: 20px;
+        }
+    }
+}
+</style>
+
+
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="page">
-        <background/>
+        <!-- <background/> -->
         <div class="main-container">
-            <div class="login-info">
+            <!-- <div class="login-info">
                 <h1>LOGIN SYSTEM</h1>
                 <van-cell-group>
                     <van-field
@@ -24,22 +24,79 @@
                     />
                     <van-button type="primary" class="btnSubmit" size="large" @click="btnLogin">登入</van-button>
                 </van-cell-group>
-            </div>
+            </div>-->
+            <van-row class="login-info text-left">
+                <van-row tag="h1">登入</van-row>
+                <van-row>
+                    <van-cell-group>
+                        <van-col tag="h5" class="title-h5" span="24">手机号</van-col>
+                        <van-field
+                            v-model="username"
+                            type="number"
+                            maxlength="11"
+                            clearable
+                            required
+                            placeholder="请输入11位手机号"
+                        />
+                        <van-col tag="h5" class="title-h5" span="24">密码</van-col>
+                        <van-field
+                            v-model="password"
+                            clearable
+                            type="password"
+                            maxlength="20"
+                            required
+                            placeholder="请输入密码"
+                        />
+                        <van-radio-group v-model="remenb.radioGroupVal">
+                            <van-radio
+                                :name="remenb.radioVal"
+                                icon-size="16"
+                                shape="square"
+                                @click="clickRemenb"
+                            >记住密码</van-radio>
+                        </van-radio-group>
+                    </van-cell-group>
+                </van-row>
+                <Vbutton buttonText="登录" @clickButton="submit"/>
+                <div class="agreement">
+                    <van-radio-group v-model="agreement.radioGroupVal">
+                        <van-radio
+                            :name="agreement.radioVal"
+                            icon-size="16"
+                            shape="square"
+                            @click="clickAgreement"
+                        >已阅读相关协议说明>></van-radio>
+                        <a href="javascript:void(0)" @click="agreementInfo">协议内容</a>
+                    </van-radio-group>
+                </div>
+            </van-row>
         </div>
     </div>
 </template>
 <script>
-import { Field } from "vant";
-import background from "@/components/background";
+import { Field, RadioGroup, Radio } from "vant";
+import button from "@/components/button";
 export default {
     components: {
         [Field.name]: Field,
-        background
+        [RadioGroup.name]: RadioGroup,
+        [Radio.name]: Radio,
+        Vbutton: button
     },
     data() {
         return {
             username: "",
-            password: ""
+            password: "",
+            remenb: {
+                radioGroupVal: false,
+                radioVal: true,
+                selectVal: false
+            },
+            agreement: {
+                radioGroupVal: false,
+                radioVal: true,
+                selectVal: false
+            }
         };
     },
     beforeRouteLeave(to, from, next) {
@@ -49,8 +106,27 @@ export default {
     created() {},
     mounted() {},
     methods: {
-        btnLogin() {
-            if (this.username && this.password) {
+        //记住密码选择
+        clickRemenb(e) {
+            this.remenb.selectVal = !this.remenb.selectVal;
+            this.remenb.radioGroupVal = this.remenb.selectVal ? true : false;
+        },
+
+        //协议内容勾选
+        clickAgreement() {
+            this.agreement.selectVal = !this.agreement.selectVal;
+            this.agreement.radioGroupVal = this.agreement.selectVal
+                ? true
+                : false;
+        },
+        //协议信息
+        agreementInfo() {
+            alert(8888);
+        },
+        //登录
+        submit() {
+            console.log("submit");
+            /* if (this.username && this.password) {
                 this.$toast("登入成功。。。");
                 this.$router.push({
                     name: "home"
@@ -60,36 +136,50 @@ export default {
             }
             setTimeout(() => {
                 this.$toast.clear();
-            }, 1000);
+            }, 1000); */
         }
     }
 };
 </script>
 <style lang='less' scoped>
 .page {
-    /deep/ .van-field__control {
-        border-bottom: 1px solid #ccc;
-    }
     .main-container {
-        padding: 0;
         .login-info {
-            width: 90%;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 10px;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            box-sizing: border-box;
-            box-shadow: 0px 10px 20px #ccc, 1px 2px 10px #0ff;
-            h1 {
-                font-size: 24px;
-                padding: 20px 0;
+            margin-top: 80px;
+            padding: 0 10px;
+            > h1 {
+                font-size: 26px;
+                padding: 10px 0;
+                margin-bottom: 20px;
             }
-            .btnSubmit {
-                border-radius: 10px;
-                margin: 20px 0;
+            .title-h5 {
+                font-size: 16px;
+                padding: 10px 0;
+            }
+            /deep/ .van-cell {
+                background-color: #f2f2f2;
+            }
+            .van-radio-group {
+                margin: 5px 0 10px 0;
+                padding: 0 10px;
+            }
+            /deep/ .van-radio__label {
+                font-size: 16px;
+                color: #666;
+            }
+            .agreement {
+                /deep/ .van-radio {
+                    display: inline-block;
+                }
+                /deep/ .van-radio-group {
+                    > a {
+                        font-size: 16px;
+                        vertical-align: middle;
+                        line-height: 20px;
+                        display: inline-block;
+                        padding-bottom: 10px;
+                    }
+                }
             }
         }
     }
