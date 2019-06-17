8bf6070d5c48d0e7ee4115a05fe44a4836a1c02e
XWQ2018 <18476969365@163.com>
2019-06-17 15:25:09 +0800

    kukukyk

--- src/api/login/login.js
+++ src/api/login/login.js
@@ -1,6 +1,6 @@
 import service from "@/untils/request";
 export default {
-	getLoginInfo(params) {
-		return service.post("/index.php", params);
-	}
+    getLoginInfo(params) {
+        return service.post("/api/login.php", params);
+    }
 };
