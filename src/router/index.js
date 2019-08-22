9a2f816095135376dbc6cac5956a6dda446b71ee
XWQ2018 <18476969365@163.com>
2019-08-22 13:55:40 +0800

    1111

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,8 @@
+/*
+ * @Description: Description
+ * @Author: xwq
+ * @Date: 2019-08-21 14:57:26
+ */
 import Vue from 'vue';
 import vueRouter from 'vue-router';
 Vue.use(vueRouter);
@@ -8,6 +13,7 @@ const userRegister = () => import('@/views/userRegister');
 const agreement = () => import('@/views/agreement');
 const torch = () => import('@/views/torch');
 const cityList = () => import('@/views/cityList');
+const test = () => import('@/views/test');
 const notFound = () => import('@/components/notFound');
 
 const router = [
@@ -84,6 +90,11 @@ const router = [
             keepAlive: true,
 
         }
+    },
+    {
+        path: '/test',
+        name: 'test',
+        component: test,
     },
     {
         path: '*',
---
+++ src/views/test.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Description: Description
+ * @Author: xwq
+ * @Date: 2019-08-21 17:28:35
+ -->
+<template>
+    <div class="tag">
+        <div class="test">
+            <van-button type="primary" class="test-btn" @click="clickHandle">节流测试</van-button>
+            <p class="desc-text">{{num}}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            num: 0
+        };
+    },
+    created() {},
+    mounted() {},
+    methods: {
+        clickHandle() {
+            setTimeout(() => {
+                this.num++;
+            }, 1000);
+        }
+    }
+};
+</script>
+<style lang="less" scoped>
+.tag {
+    .test-btn {
+        margin-top: 50px;
+    }
+    .desc-text {
+        color: #f00;
+        font-size: 26px;
+        margin-top: 50px;
+    }
+}
+</style>
