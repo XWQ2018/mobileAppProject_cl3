6cb82e3b7f3cdf275cffa2081e1cdf496f994e7f
XWQ2018 <18476969365@163.com>
2019-09-07 14:01:06 +0800

    vue $attr传参

---
+++ src/views/a.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Description: $attrr测试
+ * @Author: xwq
+ * @Date: 2019-09-07 10:46:50
+ * @LastEditTime: 2019-09-07 12:00:49
+ -->
+<template>
+    <div class="page">
+        <div>父组件的$attrs数据:{{$attrs}}</div>
+        <div>{{$listeners}}</div>
+    </div>
+</template>
+<script>
+export default {
+    components: {},
+    props: ["num"],
+    data() {
+        return {};
+    },
+    created() {},
+    mounted() {
+        console.log(this.$attrs, "******");
+    },
+    methods: {}
+};
+</script>
+<style lang="less" scoped>
+.page {
+    margin-top: 50px;
+}
+</style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -2,26 +2,48 @@
  * @Description: 测试组件
  * @Author: xwq
  * @Date: 2019-08-21 17:28:35
- * @LastEditTime: 2019-08-22 15:29:07
+ * @LastEditTime: 2019-09-07 11:41:49
  -->
 <template>
     <div class="tag">
         <div class="test">
             <van-button type="primary" class="test-btn" @click="clickHandle">测试</van-button>
-            <p class="desc-text">{{num}}</p>
+            <!-- <p class="desc-text">{{num}}</p> -->
+            <aComponent :num="num" :a="a" :b="b" v-on="$listeners" />
+            <input @input="input" v-model="value" placeholder="请输入内容" />
         </div>
     </div>
 </template>
 <script>
+import checkComponent from "./a";
 export default {
+    components: {
+        aComponent: checkComponent
+    },
     data() {
         return {
-            num: 0
+            num: 0,
+            a: 10,
+            b: 20,
+            value: ""
         };
     },
     created() {},
-    mounted() {},
-    methods: {}
+    mounted() {
+        console.log(this);
+    },
+    methods: {
+        test() {
+            this.a++;
+        },
+        input() {
+            console.log(this.value);
+        },
+        clickHandle() {
+            this.a++;
+            this.b--;
+        }
+    }
 };
 </script>
 <style lang="less" scoped>
