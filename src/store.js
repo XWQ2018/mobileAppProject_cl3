4e7c5a0f0a918c0a6bbe5e913812b40deb535267
XWQ2018 <18476969365@163.com>
2020-02-25 09:44:20 +0800

    增加vuex

--- src/store.js
+++ src/store.js
@@ -5,12 +5,21 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-
+        count: 0
+    },
+    getters: {
+        doneCount(state) {
+            return state.count + 1;
+        }
     },
     mutations: {
-
+        setShopInfoHandle(state, val) {
+            state.count = val;
+        }
     },
     actions: {
-
+        addFunc(context, val) {
+            context.commit('setShopInfoHandle', val);
+        }
     }
 });
